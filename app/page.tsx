034c3c195fbeeecab7c359f020bc27ea0a84889c
Mikilezen <mikilezen@gmail.com>
2025-11-30 02:45:55 -0800

    new

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <div className='bg'></div>
       <br />
-      <br />
       {/* <App/> */}
       <Header/>
       <Images 
