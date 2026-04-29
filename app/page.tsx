c01fae2ac275e7dd76b96aa1cbfd960c3fadc5e1
Mikilezen <mikilezen@gmail.com>
2026-04-29 06:58:18 -0700

    new

--- app/page.tsx
+++ app/page.tsx
@@ -189,7 +189,7 @@ export default function Home() {
               <a className="text-[#b7b2a6] underline decoration-[#3a3a3f] underline-offset-4" href="#contact">
                 Contact
               </a>
-              <a className="text-[#b7b2a6] underline decoration-[#3a3a3f] underline-offset-4" href="call:+251985641212">
+              <a className="text-[#b7b2a6] underline decoration-[#3a3a3f] underline-offset-4" href="tel:+251985641212">
                 Call
               </a>
             </div>
