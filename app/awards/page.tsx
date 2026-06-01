0cf0f2a06b699a72da704bd70b05397deea4ee8d
Mikiyas Zenebe Bekele <mikiyaszenebe2016@arsiun.edu.et>
2026-06-01 22:10:28 +0300

    Update page.tsx

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -7,8 +7,8 @@ const certificates = [
   {
     id: "KAGGLE-INTERMEDIATE-ML-2025",
     image: "/Mikiyas Zenebe - Intermediate Machine Learning.png",
-    url: "https://www.kaggle.com/learn/certification/mikiys/intermediate-machine-learning"
-    // url: "/Mikiyas Zenebe - Intermediate Machine Learning.png",
+    url: "https://www.kaggle.com/learn/certification/mikiys/intermediate-machine-learning",
+    // url: "/Mikiyas Zenebe - Intermediate Machine Learning.png"
     issuer: "Kaggle",
     award: "Intermediate Machine Learning",
     credential: "Issued on October 27, 2025",
